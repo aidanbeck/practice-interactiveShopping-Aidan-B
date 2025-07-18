e6e61debd6ca52cc0ae641fa205d0877c465dd25
aidanbeck <aidanmbeck@outlook.com>
2025-07-18 07:32:45 -0500

    adjusts formatting

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ const input = document.getElementById("nameInput");
 const button = document.getElementById("addItemButton");
 const list = document.getElementById("itemList");
 
-
 button.addEventListener("click", () => {
-        
+    
+    // Create Remove Button
     let removeItem = document.createElement("button");
     removeItem.innerHTML = '-';
     removeItem.addEventListener("click", function() { // Must not be an arrow function, or the 'this' keyword will not find the button. I am not sure why
         this.parentNode.remove();
     });
 
+    // Create List Element
     let li = document.createElement("li");
     li.innerHTML = input.value + " ";
     li.id = input.value;
     li.appendChild(removeItem);
-
     list.appendChild(li);
 
     input.value = '';
-
 });
